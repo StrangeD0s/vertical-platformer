165b149b88f7202934510a8a86503f9256882a22
StrangeD0s <norman.bos@gmx.de>
2023-01-22 17:27:45 +0100

    add camera comments

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
 const scaledCanvas = {
   width: canvas.width / 4,
   height: canvas.height / 4,
-}
+};
 
-const floorCollisions2D = []
+const floorCollisions2D = [];
 for (let i = 0; i < floorCollisions.length; i += 36) {
-  floorCollisions2D.push(floorCollisions.slice(i, i + 36))
+  floorCollisions2D.push(floorCollisions.slice(i, i + 36));
 }
 
-const collisionBlocks = []
+const collisionBlocks = [];
 floorCollisions2D.forEach((row, y) => {
   row.forEach((symbol, x) => {
     if (symbol === 202) {
@@ -25,17 +25,17 @@ floorCollisions2D.forEach((row, y) => {
             y: y * 16,
           },
         })
-      )
+      );
     }
-  })
-})
+  });
+});
 
-const platformCollisions2D = []
+const platformCollisions2D = [];
 for (let i = 0; i < platformCollisions.length; i += 36) {
-  platformCollisions2D.push(platformCollisions.slice(i, i + 36))
+  platformCollisions2D.push(platformCollisions.slice(i, i + 36));
 }
 
-const platformCollisionBlocks = []
+const platformCollisionBlocks = [];
 platformCollisions2D.forEach((row, y) => {
   row.forEach((symbol, x) => {
     if (symbol === 202) {
@@ -47,12 +47,12 @@ platformCollisions2D.forEach((row, y) => {
           },
           height: 4,
         })
-      )
+      );
     }
-  })
-})
+  });
+});
 
-const gravity = 0.1
+const gravity = 0.1;
 
 const player = new Player({
   position: {
@@ -61,51 +61,51 @@ const player = new Player({
   },
   collisionBlocks,
   platformCollisionBlocks,
-  imageSrc: './img/warrior/Idle.png',
+  imageSrc: "./img/warrior/Idle.png",
   frameRate: 8,
   animations: {
     Idle: {
-      imageSrc: './img/warrior/Idle.png',
+      imageSrc: "./img/warrior/Idle.png",
       frameRate: 8,
       frameBuffer: 3,
     },
     Run: {
-      imageSrc: './img/warrior/Run.png',
+      imageSrc: "./img/warrior/Run.png",
       frameRate: 8,
       frameBuffer: 5,
     },
     Jump: {
-      imageSrc: './img/warrior/Jump.png',
+      imageSrc: "./img/warrior/Jump.png",
       frameRate: 2,
       frameBuffer: 3,
     },
     Fall: {
-      imageSrc: './img/warrior/Fall.png',
+      imageSrc: "./img/warrior/Fall.png",
       frameRate: 2,
       frameBuffer: 3,
     },
     FallLeft: {
-      imageSrc: './img/warrior/FallLeft.png',
+      imageSrc: "./img/warrior/FallLeft.png",
       frameRate: 2,
       frameBuffer: 3,
     },
     RunLeft: {
-      imageSrc: './img/warrior/RunLeft.png',
+      imageSrc: "./img/warrior/RunLeft.png",
       frameRate: 8,
       frameBuffer: 5,
     },
     IdleLeft: {
-      imageSrc: './img/warrior/IdleLeft.png',
+      imageSrc: "./img/warrior/IdleLeft.png",
       frameRate: 8,
       frameBuffer: 3,
     },
     JumpLeft: {
-      imageSrc: './img/warrior/JumpLeft.png',
+      imageSrc: "./img/warrior/JumpLeft.png",
       frameRate: 2,
       frameBuffer: 3,
     },
   },
-})
+});
 
 const keys = {
   d: {
@@ -114,34 +114,34 @@ const keys = {
   a: {
     pressed: false,
   },
-}
+};
 
 const background = new Sprite({
   position: {
     x: 0,
     y: 0,
   },
-  imageSrc: './img/background.png',
-})
+  imageSrc: "./img/background.png",
+});
 
-const backgroundImageHeight = 432
+const backgroundImageHeight = 432;
 
 const camera = {
   position: {
     x: 0,
     y: -backgroundImageHeight + scaledCanvas.height,
   },
-}
+};
 
 function animate() {
-  window.requestAnimationFrame(animate)
-  c.fillStyle = 'white'
-  c.fillRect(0, 0, canvas.width, canvas.height)
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
-  c.save()
-  c.scale(4, 4)
-  c.translate(camera.position.x, camera.position.y)
-  background.update()
+  c.save();
+  c.scale(4, 4);
+  c.translate(camera.position.x, camera.position.y);
+  background.update();
   // collisionBlocks.forEach((collisionBlock) => {
   //   collisionBlock.update()
   // })
@@ -150,61 +150,64 @@ function animate() {
   //   block.update()
   // })
 
-  player.checkForHorizontalCanvasCollision()
-  player.update()
+  player.checkForHorizontalCanvasCollision();
+  player.update();
 
-  player.velocity.x = 0
+  player.velocity.x = 0;
   if (keys.d.pressed) {
-    player.switchSprite('Run')
-    player.velocity.x = 2
-    player.lastDirection = 'right'
-    player.shouldPanCameraToTheLeft({ canvas, camera })
+    player.switchSprite("Run");
+    // * Hier ist die velocity, die in der "shouldPanCameraToTheLeft" benutzt wird.
+    player.velocity.x = 2;
+    player.lastDirection = "right";
+    // * "shouldPanCameraToTheLeft()" wird aufgerufen, wenn "d" gedrücked wird.
+    player.shouldPanCameraToTheLeft({ canvas, camera });
   } else if (keys.a.pressed) {
-    player.switchSprite('RunLeft')
-    player.velocity.x = -2
-    player.lastDirection = 'left'
-    player.shouldPanCameraToTheRight({ canvas, camera })
+    player.switchSprite("RunLeft");
+    player.velocity.x = -2;
+    player.lastDirection = "left";
+    player.shouldPanCameraToTheRight({ canvas, camera });
   } else if (player.velocity.y === 0) {
-    if (player.lastDirection === 'right') player.switchSprite('Idle')
-    else player.switchSprite('IdleLeft')
+    if (player.lastDirection === "right") player.switchSprite("Idle");
+    else player.switchSprite("IdleLeft");
   }
 
   if (player.velocity.y < 0) {
-    player.shouldPanCameraDown({ camera, canvas })
-    if (player.lastDirection === 'right') player.switchSprite('Jump')
-    else player.switchSprite('JumpLeft')
+    player.shouldPanCameraDown({ camera, canvas });
+    if (player.lastDirection === "right") player.switchSprite("Jump");
+    else player.switchSprite("JumpLeft");
   } else if (player.velocity.y > 0) {
-    player.shouldPanCameraUp({ camera, canvas })
-    if (player.lastDirection === 'right') player.switchSprite('Fall')
-    else player.switchSprite('FallLeft')
+    player.shouldPanCameraUp({ camera, canvas });
+    if (player.lastDirection === "right") player.switchSprite("Fall");
+    else player.switchSprite("FallLeft");
   }
 
-  c.restore()
+  c.restore();
+  renderDevInfos();
 }
 
-animate()
+animate();
 
-window.addEventListener('keydown', (event) => {
+window.addEventListener("keydown", (event) => {
   switch (event.key) {
-    case 'd':
-      keys.d.pressed = true
-      break
-    case 'a':
-      keys.a.pressed = true
-      break
-    case 'w':
-      player.velocity.y = -4
-      break
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "w":
+      player.velocity.y = -4;
+      break;
   }
-})
+});
 
-window.addEventListener('keyup', (event) => {
+window.addEventListener("keyup", (event) => {
   switch (event.key) {
-    case 'd':
-      keys.d.pressed = false
-      break
-    case 'a':
-      keys.a.pressed = false
-      break
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
   }
-})
+});
--- js/utils.js
+++ js/utils.js
@@ -4,7 +4,7 @@ function collision({ object1, object2 }) {
     object1.position.y <= object2.position.y + object2.height &&
     object1.position.x <= object2.position.x + object2.width &&
     object1.position.x + object1.width >= object2.position.x
-  )
+  );
 }
 
 function platformCollision({ object1, object2 }) {
@@ -14,5 +14,40 @@ function platformCollision({ object1, object2 }) {
       object2.position.y + object2.height &&
     object1.position.x <= object2.position.x + object2.width &&
     object1.position.x + object1.width >= object2.position.x
-  )
+  );
+}
+
+// * Renderfunktion für DevInfos
+function renderDevInfos() {
+  const lineHeight = 25;
+
+  const player_x = player.position.x;
+  const player_y = player.position.y;
+  const player_velocity_x = player.velocity.x;
+  const player_velocity_y = player.velocity.y;
+  const player_width = player.width;
+  const camerabox_x = player.camerabox.position.x;
+  const camerabox_y = player.camerabox.position.y;
+  const camerabox_width = player.camerabox.width;
+  const camerabox_height = player.camerabox.height;
+
+  c.font = "20px Arial";
+  c.textAlign = "left";
+  c.fillStyle = "hotpink";
+  c.fillText("player_x: " + player_x.toFixed(2), 50, lineHeight * 2);
+  c.fillText("player_y: " + player_y.toFixed(2), 50, lineHeight * 3);
+  c.fillStyle = "papayawhip";
+  c.fillText("player_velocity_x: " + player_velocity_x, 50, lineHeight * 4);
+  c.fillText(
+    "player_velocity_y: " + player_velocity_y.toFixed(2),
+    50,
+    lineHeight * 5
+  );
+
+  c.fillText("camerabox_x: " + camerabox_x.toFixed(2), 50, lineHeight * 6);
+  c.fillText("camerabox_y: " + camerabox_y.toFixed(2), 50, lineHeight * 7);
+
+  c.fillText("camerabox_width: " + camerabox_width, 50, lineHeight * 8);
+  c.fillText("camerabox_height: " + camerabox_height, 50, lineHeight * 9);
+  c.fillText("player_width: " + player_width, 50, lineHeight * 10);
 }
